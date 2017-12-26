dfe1703fc7f875efdb5cd81c7698d14bcc21f61b
amrelmasry <amrelmasry.me@gmail.com>
2017-12-26 13:16:25 +0200

    Update documentation

--- domain/src/main/java/org/buffer/android/boilerplate/domain/interactor/FlowableUseCase.kt
+++ domain/src/main/java/org/buffer/android/boilerplate/domain/interactor/FlowableUseCase.kt
@@ -1,7 +1,6 @@
 package org.buffer.android.boilerplate.domain.interactor
 
 import io.reactivex.Flowable
-import io.reactivex.Single
 import io.reactivex.schedulers.Schedulers
 import org.buffer.android.boilerplate.domain.executor.PostExecutionThread
 import org.buffer.android.boilerplate.domain.executor.ThreadExecutor
@@ -9,14 +8,14 @@ import org.buffer.android.boilerplate.domain.executor.ThreadExecutor
 
 
 /**
- * Abstract class for a UseCase that returns an instance of a [Single].
+ * Abstract class for a UseCase that returns an instance of a [Flowable].
  */
 abstract class FlowableUseCase<T, in Params> constructor(
         private val threadExecutor: ThreadExecutor,
         private val postExecutionThread: PostExecutionThread) {
 
     /**
-     * Builds a [Single] which will be used when the current [FlowableUseCase] is executed.
+     * Builds a [Flowable] which will be used when the current [FlowableUseCase] is executed.
      */
     protected abstract fun buildUseCaseObservable(params: Params? = null): Flowable<T>
 
