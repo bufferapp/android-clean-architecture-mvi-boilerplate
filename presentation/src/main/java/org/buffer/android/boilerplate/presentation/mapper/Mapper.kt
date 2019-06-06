0bee5d435ac45cce3f3852f802d4518492f6d634
Luca Nicoletti <luca.nicolett@gmail.com>
2019-06-06 20:58:58 +0100

    Update documentation in Mapper.kt
    Fix input/output in the documentation, were written on the wrong parameter

--- presentation/src/main/java/org/buffer/android/boilerplate/presentation/mapper/Mapper.kt
+++ presentation/src/main/java/org/buffer/android/boilerplate/presentation/mapper/Mapper.kt
@@ -4,11 +4,16 @@ package org.buffer.android.boilerplate.presentation.mapper
  * Interface for model mappers. It provides helper methods that facilitate
  * retrieving of models from outer layers
  *
- * @param <V> the view model input type
- * @param <D> the domain model output type
+ * @param <V> the view model output type
+ * @param <D> the domain model input type
  */
 interface Mapper<out V, in D> {
 
+    /**
+     * Function to map a domain model to a view model
+     * @param type input domain model
+     * @return <V> the respective view model type
+     */
     fun mapToView(type: D): V
 
 }
